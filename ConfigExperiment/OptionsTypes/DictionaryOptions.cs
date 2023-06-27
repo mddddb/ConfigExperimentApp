bf7ccfe12d3456d06f845bc04afebf4c6ec06c56
Murat Duisenbayev <mduisenbayev@microsoft.com>
2023-06-27 17:28:25 +0200

    Update DictionaryOptions.cs

--- ConfigExperiment/OptionsTypes/DictionaryOptions.cs
+++ ConfigExperiment/OptionsTypes/DictionaryOptions.cs
@@ -2,6 +2,12 @@
 
 namespace ConfigExperiment;
 
+/// <summary>
+/// Options type with a dictionary property, where the values are polymorphic.
+/// </summary>
+/// <remarks>
+/// Each IConfigurationSection mapped to IOptionsBase has "_Type" property to identify the exact implementation of IOptionsBase
+/// </remarks>
 public class DictionaryOptions
 {
     public string SomeRegularProperty { get; set; }
