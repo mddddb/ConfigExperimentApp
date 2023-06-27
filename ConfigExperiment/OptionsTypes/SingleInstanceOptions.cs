3e8ab8a55f8259a0d14bc136bc0dada09eab039c
Murat Duisenbayev <mduisenbayev@microsoft.com>
2023-06-27 17:28:01 +0200

    Update SingleInstanceOptions.cs

--- ConfigExperiment/OptionsTypes/SingleInstanceOptions.cs
+++ ConfigExperiment/OptionsTypes/SingleInstanceOptions.cs
@@ -2,10 +2,12 @@
 
 namespace ConfigExperiment;
 
+/// <summary>
+/// Options type with a simple property, where the value is polymorphic.
+/// </summary>
 public class SingleInstanceOptions
 {
     public string SomeRegularProperty { get; set; }
-
 
     [ConfigurationKeyName(nameof(Item))]
     internal IConfigurationSection ItemAsSection { get; set; }
