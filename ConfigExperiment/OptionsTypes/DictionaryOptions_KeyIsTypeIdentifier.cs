0df725427c3438be319c5a20c85c359533a0a485
Murat Duisenbayev <mduisenbayev@microsoft.com>
2023-06-27 17:28:36 +0200

    Update DictionaryOptions_KeyIsTypeIdentifier.cs

--- ConfigExperiment/OptionsTypes/DictionaryOptions_KeyIsTypeIdentifier.cs
+++ ConfigExperiment/OptionsTypes/DictionaryOptions_KeyIsTypeIdentifier.cs
@@ -2,6 +2,12 @@
 
 namespace ConfigExperiment;
 
+/// <summary>
+/// Options type with a dictionary property, where the values are polymorphic.
+/// </summary>
+/// <remarks>
+/// The exact implementation of IOptionsBase is identified by the key of each KeyValuePair.
+/// </remarks>
 public class DictionaryOptions_KeyIsTypeIdentifier
 {
     public string SomeRegularProperty { get; set; }
